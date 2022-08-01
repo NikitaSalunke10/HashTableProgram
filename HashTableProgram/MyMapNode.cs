355fc9f1ae7e99433f94f2f4fa3b02fcab269a15
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-01 17:27:30 +0530

    Add project files.

---
+++ HashTableProgram/MyMapNode.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProgram
+{
+    internal class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);  // |-5| =5 |3|=3 |-3|=3
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        // 5-4345 7-8765456 8-8745
+        protected int GetArrayPosition(K key)// 5->-7654323456     5->  7654323456 //78-87654568745 
+        { // 100 ->876543456787654  -> 100->876543456787654
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+        public void FindFrequency(string[] words) // this method is used to find the count of each word
+        {
+            Dictionary<string, int> WordFrequency = new Dictionary<string, int>();
+            foreach (string word in words) // foreach loop is used to get one value at a time
+            {
+                if (WordFrequency.ContainsKey(word)) // this condition is to check whether the dictionary already contains the word or not
+                {// if condition becomes true it means that word is already present and then the value is increment by 1
+                    WordFrequency[word] += 1;
+                }
+                else // if condition becomes false it means that the word is occured one time only
+                {
+                    WordFrequency.Add(word, 1);
+                }
+            }
+            Console.WriteLine("\nThe Frequency of Words are: \nWord  Count");
+            foreach (var word in WordFrequency) // foreach loop is used to print the dictionary
+            {
+                Console.WriteLine(word.Key + "     " + word.Value);
+            }
+        }
+    }
+}
---
+++ HashTableProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using HashTableProgram;
+
+Console.WriteLine("Hash Table Program\n");
+MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+hash.Add("0", "To");
+hash.Add("1", "be");
+hash.Add("2", "or");
+hash.Add("3", "not");
+hash.Add("4", "to");
+hash.Add("5", "be");
+string hash5 = hash.Get("5");
+Console.WriteLine("5th index value: " + hash5);
+string hash2 = hash.Get("2");
+Console.WriteLine("2th index value: " + hash2);
+string[] words = new string[6];
+for (int i = 0; i < words.Length; i++)// for loop is used to store the hash values in string array
+{
+    string key = i.ToString();
+    words[i] = hash.Get(key).ToLower(); // one by one the values are retrived and store at each key
+}
+hash.FindFrequency(words); //method is called to find the count of words
